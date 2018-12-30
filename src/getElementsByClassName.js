40d7b009ebcc1043afae7a89ffde86c9325d471f
NickRashad <nrsternes@gmail.com>
2018-12-30 02:08:12 -0600

    Update minor syntax

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,25 +6,24 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className) {
   var result = [];
-  function diveIn(document){
+  function getEl(document){
     for (var i = 0; i < document.childNodes.length; i++) {
       var classes = document.childNodes[i].classList;
       if (classes && classes.contains(className)) {
         result.push(document.childNodes[i]);
       }
       if (document.childNodes[i].childNodes[0]) {
-        diveIn(document.childNodes[i]);
+        getEl(document.childNodes[i]);
       }
     }
   }
-  diveIn(document);
+  getEl(document);
   return result;
 };
 /* 
   console.log(document.getElementsByClassName('targetClassName')) 
-        => [body.targetClassName, div.targetClassName]
-  
-   console.log( document.body.classList[0] ) => targetClassName
-   console.log( document.body.childNodes ) => [text, div#mocha, text, div.targetClassName]
+    => [body.targetClassName, div.targetClassName]
+  console.log( document.body.classList[0] ) => targetClassName
+  console.log( document.body.childNodes ) => [text, div#mocha, text, div.targetClassName]
   //https://codereview.stackexchange.com/questions/91181/practicing-recursion-with-getelementsbyclassname
-  */
+*/
