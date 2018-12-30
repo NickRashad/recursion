bfe128ce85b37b65d54cfbdded55c48e36c4c60d
NickRashad <nrsternes@gmail.com>
2018-12-30 02:07:45 -0600

    Pass conditions for number, string, boolean, array, and null

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,35 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (obj[0] === undefined){
+    return [];
+  } else if(typeof obj[0] === "number" || typeof obj[0] === "boolean"){
+    return [obj[0]].concat( stringifyJSON(obj.slice(1)));
+  } else if(obj[0] === null){
+    return [null].concat( stringifyJSON(obj.slice(1)) );
+  } else if(typeof obj[0] === "string"){
+    var singlefy = obj[0];
+    singlefy = singlefy.replace(/'/g, '"');
+    return [ singlefy ].concat( stringifyJSON(obj.slice(1)) );
+  } else if(Array.isArray(obj[0])) {
+    return [stringifyJSON(obj[0])].concat( stringifyJSON(obj.slice(1)) );
+  } 
 };
+
+  /*if(obj[0] === undefined){
+    return [];
+  } else {
+    if (Array.isArray(obj[0])){
+      return stringifyJSON(obj[0]).concat( stringifyJSON(obj.slice(1)) );
+    } else {
+      return obj[0].concat( stringifyJSON(obj.slice(1)) );
+    }
+  }*/
+  /*var result = [];
+  function stringify(obj){
+    for (var i = 0; i < obj.length; i++) {
+      
+    }
+  }
+  stringify(obj);
+  return result;*/
