608b5a665d3802416e6b97f4926dc37f941af4c4
NickRashad <nrsternes@gmail.com>
2018-12-31 14:43:19 -0600

    Complete JSON logic for Objects

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -7,32 +7,47 @@ var stringifyJSON = function(obj) {
   if (obj[0] === undefined){
     return [];
   } else if(typeof obj[0] === "number" || typeof obj[0] === "boolean"){
-    return [obj[0]].concat( stringifyJSON(obj.slice(1)));
+    return [obj[0]].concat( stringifyJSON(obj.slice(1)) );
   } else if(obj[0] === null){
     return [null].concat( stringifyJSON(obj.slice(1)) );
   } else if(typeof obj[0] === "string"){
-    var singlefy = obj[0];
-    singlefy = singlefy.replace(/'/g, '"');
-    return [ singlefy ].concat( stringifyJSON(obj.slice(1)) );
+    return [ obj[0] ].concat( stringifyJSON(obj.slice(1)) );
   } else if(Array.isArray(obj[0])) {
     return [stringifyJSON(obj[0])].concat( stringifyJSON(obj.slice(1)) );
-  } 
+  } else if(typeof obj[0] === "object"){
+    var newObj = {};
+    for(var key in obj[0]){
+      newObj['"' + key + '"'] = obj[0][key];
+    }
+    return [newObj].concat( stringifyJSON(obj.slice(1)) );
+  }
 };
 
-  /*if(obj[0] === undefined){
-    return [];
-  } else {
-    if (Array.isArray(obj[0])){
-      return stringifyJSON(obj[0]).concat( stringifyJSON(obj.slice(1)) );
-    } else {
-      return obj[0].concat( stringifyJSON(obj.slice(1)) );
-    }
-  }*/
-  /*var result = [];
-  function stringify(obj){
-    for (var i = 0; i < obj.length; i++) {
-      
-    }
-  }
-  stringify(obj);
-  return result;*/
+/*
+var letSee = stringifyJSON([
+  9,
+  null,
+  true,
+  false,
+  'Hello world',
+  8,
+  [],
+  [8],
+  ['hi'],
+  [8, 'hi'],
+  [1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999],
+  [8, [[], 3, 4]],
+  [[[['foo']]]],
+  {},
+  {'a': 'apple'},
+  {'foo': true, 'bar': false, 'baz': null},
+  {'boolean, true': true, 'boolean, false': false, 'null': null },
+  // basic nesting
+  {'a': {'b': 'c'}},
+  {'a': ['b', 'c']},
+  [{'a': 'b'}, {'c': 'd'}],
+  {'a': [], 'c': {}, 'b': true}
+  ]);
+
+  console.log(letSee);  
+*/
